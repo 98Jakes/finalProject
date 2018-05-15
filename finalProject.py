4484939ffb8217c7a817683ab81b36e1f89e3017
strydr <strydr8@gmail.com>
2018-05-15 11:42:36 -0700

    progress

--- finalProject.py
+++ finalProject.py
@@ -1,8 +1,19 @@
 import string
-def randomNumAssignment(numLetter):
+import random
+
+def generateRandomNumber(numLetter):
+    
+    
+def randomNumAssignment(numLetter, randInt):
     lowerCase = string.ascii_lowercase
     print(lowerCase[numLetter])
+    
 def main():
+    userSeed = int(input("Please enter a seed number, an integer."))
+    random.seed(userSeed)
+    
+    userString = str(input("Please enter the message you want to encrypt."))
+    
     for x in range(0, 26):
         randomNumAssignment(x)
     
